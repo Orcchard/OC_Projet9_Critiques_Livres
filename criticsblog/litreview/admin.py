008b5bb7d4e2df150f7796b1616a0d1778cb78d2
Dahbia Zaidi <orchard@noos.fr>
2026-05-05 23:15:08 +0200

    05-05-2026 corrections admin

--- criticsblog/litreview/admin.py
+++ criticsblog/litreview/admin.py
@@ -3,11 +3,25 @@ from .models import Ticket, Review
 
 
 class TicketAdmin(admin.ModelAdmin):
-    list_display = ('title', 'description', 'user', 'image', 'time_created')
+    list_display = ('id', 'title', 'user', 'image', 'time_created')
 
 
 class ReviewAdmin(admin.ModelAdmin):
-    list_display = ('time_created', 'ticket', 'user')
+    list_display = (
+        'id',
+        'headline',
+        'ticket_id_display',
+        'ticket',
+        'user',
+        'time_created'
+    )
+
+    readonly_fields = ('id',)
+
+    def ticket_id_display(self, obj):
+        return obj.ticket.id
+
+    ticket_id_display.short_description = "Ticket ID"
 
 
 admin.site.register(Ticket, TicketAdmin)
