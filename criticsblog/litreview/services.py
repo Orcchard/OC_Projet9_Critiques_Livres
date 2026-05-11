14607b39f47ad175cbc147caf286d32420606435
Dahbia Zaidi <orchard@noos.fr>
2026-05-11 23:07:46 +0200

    Ajout dans services.py methodes my publications du 11-05-2026

--- criticsblog/litreview/services.py
+++ criticsblog/litreview/services.py
@@ -1,5 +1,7 @@
+from django.db.models import Exists, OuterRef, Value, CharField
 from litreview.models import Ticket, Review
 from authentication.models import UserFollow
+from django.db import models
 
 
 def get_users_viewable_tickets(user):
@@ -26,17 +28,35 @@ def get_users_viewable_tickets(user):
     # Ajouter à chaque Ticket : ticket.has_review, tickets avec review
     # OuterRef('pk') le ticket actuel de la requête principale
 
-def get_users_viewable_reviews(user):
-    """
-    Reviews visibles :
-    - celles du request.user
-    - celles des utilisateurs suivis
-    """
 
+def get_users_viewable_reviews(user):
     followed_users = UserFollow.objects.filter(
         user=user
     ).values_list('followed_user', flat=True)
 
     allowed_users = list(followed_users) + [user.id]
 
-    return Review.objects.filter(user_id__in=allowed_users)
+    return Review.objects.filter(
+        user_id__in=allowed_users
+    ).annotate(
+        content_type=models.Value('REVIEW', models.CharField())
+    )
+
+
+def get_user_tickets(user):
+    return Ticket.objects.filter(
+        user=user
+    ).annotate(
+        content_type=Value('TICKET', CharField()),
+        has_review=Exists(
+            Review.objects.filter(ticket_id=OuterRef('pk'))
+        )
+    )
+
+
+def get_user_reviews(user):
+    return Review.objects.filter(
+        user=user
+    ).annotate(
+        content_type=Value('REVIEW', CharField())
+    )
