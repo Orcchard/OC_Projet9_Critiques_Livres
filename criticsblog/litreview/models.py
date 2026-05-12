f3f4c2fb99deaafba706c69ff6ccb2651d200977
Dahbia Zaidi <orchard@noos.fr>
2026-05-12 12:27:34 +0200

    modifs models.py 12-05-2026

--- criticsblog/litreview/models.py
+++ criticsblog/litreview/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.conf import settings
 from django.core.validators import MinValueValidator, MaxValueValidator
 from django.utils import timezone
-# from .models import Review
 
 
 class Ticket(models.Model):
